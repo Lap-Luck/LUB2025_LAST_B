61a3187d5b4b75344da14804567eb8c8e07ae3b0
MichalBiesek <michal.biesek@itsilesia.com>
2025-01-26 01:43:35 +0100

    fish new

--- module/game/public/actor/FishActor.hpp
+++ module/game/public/actor/FishActor.hpp
@@ -7,6 +7,9 @@ public:
     FishActor(GameState& inState,Vec2f inPos ) : Actor(inState,inPos){};
     ~FishActor(){};
 
+    Obstacle::ObstacleId id_body;
+    Obstacle::ObstacleId id_kill;
+
 
     void onPlaced() override
     {
@@ -16,8 +19,9 @@ public:
         state.assets.fish2=cmasks[1];
 
         Obstacle body = Obstacle(&state.assets.fish1,{pos.x,pos.y},10.0f);
-
+        id_body=body.unique_id;
         Obstacle kill = Obstacle(&state.assets.fish2,{pos.x,pos.y},10.0f);
+        id_kill=kill.unique_id;
         kill.onHit = [](Bubble* bubble)
         {
             bubble->flags.pendingDestroy = true;
@@ -27,10 +31,15 @@ public:
         state.obstacles.values.push_back(std::move(kill));
 
     }
-
-    void onDraw() override {
+    void onUpdate() override {
         pos.y+=0.15f;
         pos.x-=0.5f;
+        state.obstacles.getById(id_body)->pos=pos;
+        state.obstacles.getById(id_kill)->pos=pos;
+    }
+
+    void onDraw() override {
+
         DrawTextureEx(state.assets.szczupakf, {pos.x,pos.y}, 0.0f,1.0f,WHITE);
     }
 
